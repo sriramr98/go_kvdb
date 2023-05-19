49e26c3471c13a96bed01269f1c2bf81aedc08ca
Sriram R <sriramr083@gmail.com>
2023-05-19 18:56:09 +0530

    Protocol ( Dev - Master ) (#13)
    Added Protocol Parser and completed basic TCP flow with parsing and message processing

---
+++ core/protocol.go
@@ -0,0 +1,70 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var ErrInvalidCommand = errors.New("invalid command")
+
+type Request struct {
+	Command Command
+	Params  []string
+}
+
+type Response struct {
+	Success bool
+	Value   []byte
+}
+
+type Command struct {
+	Cmd               string
+	MinRequiredParams int
+}
+
+var (
+	CMDGet  Command = Command{Cmd: "GET", MinRequiredParams: 1}
+	CMDSet  Command = Command{Cmd: "SET", MinRequiredParams: 2}
+	CMDDel  Command = Command{Cmd: "DEL", MinRequiredParams: 1}
+	CMDPing Command = Command{Cmd: "PING", MinRequiredParams: 0}
+)
+
+func (c Command) isParamsValid(params []string) bool {
+	return len(params) >= c.MinRequiredParams
+}
+
+func parseCommand(cmd string) (Command, error) {
+	switch cmd {
+	case CMDGet.Cmd:
+		return CMDGet, nil
+	case CMDSet.Cmd:
+		return CMDSet, nil
+	case CMDDel.Cmd:
+		return CMDDel, nil
+	case CMDPing.Cmd:
+		return CMDPing, nil
+	default:
+		return Command{}, ErrInvalidCommand
+	}
+}
+
+func ParseProtocol(input string) (Request, error) {
+	input_parts := strings.Split(input, " ")
+	fmt.Println(len(input_parts))
+	if len(input_parts) == 0 {
+		return Request{}, errors.New("empty request")
+	}
+
+	command, err := parseCommand(input_parts[0])
+	if err != nil {
+		return Request{}, err
+	}
+	params := input_parts[1:]
+
+	if !command.isParamsValid(params) {
+		return Request{}, errors.New("invalid params")
+	}
+
+	return Request{Command: command, Params: params}, nil
+}
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"gitub.com/sriramr98/go_kvdb/core"
+	"gitub.com/sriramr98/go_kvdb/store"
 )
 
 func main() {
-	core.NewServer(core.ServerOpts{Port: 8080}).Start()
+	inmemstore := store.NewInMemoryStore()
+	processor := &core.CommandProcessor{Store: inmemstore}
+	core.NewServer(core.ServerOpts{Port: 8080}, processor).Start()
 }
