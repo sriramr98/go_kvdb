2c2328d4dbfe9e06fcc4f2e491cbbe626f8362b8
R.Sriram <r.sriram@games24x7.com>
2023-05-26 11:09:52 +0530

    small fix

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"gitub.com/sriramr98/go_kvdb/core"
+	"gitub.com/sriramr98/go_kvdb/core/processors"
 	"gitub.com/sriramr98/go_kvdb/core/protocol"
 	"gitub.com/sriramr98/go_kvdb/store"
 )
 
 func main() {
 	inmemstore := store.NewInMemoryStore()
-	processor := &core.CommandProcessor{Store: inmemstore}
+	processor := &processors.CommandProcessor{Store: inmemstore}
 	protcol := protocol.ClientProtocol{}
 	core.NewServer(core.ServerOpts{Port: 8080}, processor, protcol).Start()
 }
